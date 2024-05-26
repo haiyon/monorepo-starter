ef2e0ac7579fa518e9b2b64146e5dd08b42d196a
Shen <ihaiyon@gmail.com>
2024-05-26 17:14:15 +0800

    tailwind classes merge method & update shell layout

--- web/src/components/layout/page.tsx
+++ web/src/components/layout/page.tsx
@@ -1,7 +1,3 @@
 import React from 'react';
 
-import { Shell } from '@/components/shell/shell';
-
-export const Page = ({ children }: { children: React.ReactNode }) => {
-  return <Shell>{children}</Shell>;
-};
+export const Page = ({ children }: { children: React.ReactNode }) => children;
